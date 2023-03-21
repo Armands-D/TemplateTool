51bab8870d00935a5e36945df9e3c82ea41194e3
Armands <armands.dunskis2@mail.dcu.ie>
2023-03-21 14:23:56 +0000

    added get copying

--- makefile
+++ makefile
@@ -1,5 +1,15 @@
+SHELL:= /sbin/zsh
 
-install-pack: 
+all: clean-venv virtual-install-pack source-rc
+
+clean-venv:
+	rm -rf venv
+	rm -rf *.egg*
+
+source-rc:
+	source ~/.zshrc
+
+virtual-install-pack: 
 	virtualenv venv
 	. venv/bin/activate
 	pip install --editable .
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='main',
+    name='temp_tool',
     version='0.1.0',
-    py_modules=['main'],
+    py_modules=['temp_tool'],
     install_requires=[
         'Click',
     ],
     entry_points={
         'console_scripts': [
-            'main = main:cli',
+            'temp_tool = temp_tool:cli',
         ],
     },
 )
